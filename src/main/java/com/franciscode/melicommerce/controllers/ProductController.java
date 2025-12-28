fbed2a35879c7eccd865d8988c14febfc115b5d6
Francisco Correia <felipedreamcorreia@hotmail.com>
2025-12-28 13:41:21 -0300

    feature: find all products with pageable

--- src/main/java/com/franciscode/melicommerce/controllers/ProductController.java
+++ src/main/java/com/franciscode/melicommerce/controllers/ProductController.java
@@ -3,12 +3,12 @@ package com.franciscode.melicommerce.controllers;
 import com.franciscode.melicommerce.dto.ProductDTO;
 import com.franciscode.melicommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -23,7 +23,7 @@ public class ProductController {
     }
 
     @GetMapping
-    public List<ProductDTO> findAll() {
-        return service.findAll();
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 }
--- src/main/java/com/franciscode/melicommerce/services/ProductService.java
+++ src/main/java/com/franciscode/melicommerce/services/ProductService.java
@@ -4,10 +4,10 @@ import com.franciscode.melicommerce.dto.ProductDTO;
 import com.franciscode.melicommerce.entities.Product;
 import com.franciscode.melicommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class ProductService {
@@ -22,8 +22,8 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public List<ProductDTO> findAll() {
-        List<Product> result = repository.findAll();
-        return result.stream().map(ProductDTO::new).toList();
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(ProductDTO::new);
     }
 }
